938d4d31d63ffca5b0d401df9e35697a3a9436fd
Ronit <ronitkbiswas@gmail.com>
2026-03-12 23:35:11 +0530

    little changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,30 @@ function Ronit() {
   const currentYear = new Date().getFullYear();
   const list = me.map((ronit) => (
     <span key={me.id}>
-      <img src={ronit.imageUrl} alt={ronit.name} style={{ width: "150px" }} />
-      <br></br>
-      <br></br>
-      <div>
+      <div
+        style={{
+          textAlign: "center",
+          paddingTop: "50px",
+          borderRadius: "40px",
+        }}
+      >
+        <img src={ronit.imageUrl} alt={ronit.name} style={{ width: "150px" }} />
+      </div>
+      <div style={{ textAlign: "center", fontFamily: "cursive" }}>
         <p className="intro">
-          Hello 👋 <br></br> I'm {ronit.name} 🧑‍💻 gearing up to become a{" "}
-          {ronit.profession} soon 😎💻 I'm from {ronit.city} 🏠 <br></br> I have
-          done {ronit.degreeIn} ({ronit.highestEducation}, {ronit.passoutYear}{" "}
-          passout.) 🎓 I have {ronit.accomplishment} (JB's Flame) which are
-          doing pretty good (meaning profitable!) 🔥 I am currently{" "}
+          <span style={{ fontSize: "25px" }}>Hello 👋</span>
+          <br></br> I'm <strong>{ronit.name}</strong> 🧑‍💻 gearing up to become a{" "}
+          {ronit.profession} soon 😎💻
+          <br></br>
+          <br></br> I'm from {ronit.city} 🏠 <br></br> I have done{" "}
+          {ronit.degreeIn} ({ronit.highestEducation}, {ronit.passoutYear}{" "}
+          passout.) 🎓
+          <br></br> <br></br> I have {ronit.accomplishment} (JB's Flame) which
+          are doing pretty good (meaning profitable!) 🔥 I am currently{" "}
           {currentYear - ronit.birthYear} years old as of {currentYear} 👨
           <br></br>
-          I've made this entire web app in react JS! 😎🍾
+          <br></br>
+          <span>I've made this entire web app in react JS! 😎🍾</span>
         </p>
       </div>
     </span>
