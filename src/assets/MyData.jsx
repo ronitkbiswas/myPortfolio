7d117aefbbc19ffb73bcb9fa573a5443677a27ce
Ronit Kumar Biswas <ronitkbiswas@gmail.com>
2026-03-18 11:28:12 +0530

    basic done

--- src/assets/MyData.jsx
+++ src/assets/MyData.jsx
@@ -1,14 +1,18 @@
 function Bio() {
+  const currentYear = new Date().getFullYear();
+  const birthYear = 1995;
+
   return (
     <>
       <div style={{ backgroundColor: "black", padding: "10px" }}>
         <span style={{ fontFamily: "monospace" }}>
           <span style={{ fontSize: "20px" }}>Hello 👋</span>
           <br></br> I'm Ronit Kumar Biswas 🧑‍💻 gearing up to become a ReactJS Dev
-          soon 😎💻<br></br> I'm from Kolkata 🏠 I have done Applied Electronis
-          & Intrumentation Engineering (B.Tech, 2017 passout.) 🎓 I have Founded
-          a Restaurant (JB's Flame) which are doing pretty good (meaning
-          profitable!) 🔥<br></br> I am currently 31 years old as of 2026 👨
+          soon 😎💻<br></br> From Kolkata 🏠 Applied Electronis & Intrumentation
+          Engineer (B.Tech, 2017 passout.) 🎓 <br></br>Have Founded a Restaurant
+          (JB's Flame) which are doing pretty good (meaning profitable!) 🔥
+          <br></br> Currently {currentYear - birthYear} years old as of{" "}
+          {currentYear}👨
           <br></br>
           <br></br>
           <span>I've made this entire web app in react JS! 😎🍾</span>
