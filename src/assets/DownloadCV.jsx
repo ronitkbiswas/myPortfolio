49abb5a17c3cb2bd91f0877dcc9d24bd6a9a1280
Ronit Kumar Biswas <ronitkbiswas@gmail.com>
2026-03-18 15:41:22 +0530

    changes

--- src/assets/DownloadCV.jsx
+++ src/assets/DownloadCV.jsx
@@ -1,10 +1,15 @@
 import "/src/App.css";
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+
 function DownloadCV() {
+  const notify = () => toast("CV not available for download yet!");
   return (
     <>
-      <button className="downloadCVButton" type="button">
+      <button className="downloadCVButton" type="button" onClick={notify}>
         Download my Resume (not available yet)
       </button>
+      <ToastContainer />
     </>
   );
 }
