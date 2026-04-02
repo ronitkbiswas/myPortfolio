693ed714b791db908dcbeff9b9c25ad473cb6a14
Ronit <ronitkbiswas@gmail.com>
2026-04-02 10:57:11 +0530

    cv

--- src/components/MyPhoto.jsx
+++ src/components/MyPhoto.jsx
@@ -2,7 +2,7 @@ function Photo() {
   return (
     <>
       <img
-        src="/myPhotos/myDP.jpg"
+        src="/images/myDP.jpg"
         alt="my profile pic"
         width="170px"
         style={{ borderRadius: "100px", boxShadow: "1px 1px 10px 1px" }}
