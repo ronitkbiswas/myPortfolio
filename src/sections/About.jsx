aeee4c404d0cafb4ca103a8c37155987b5d61447
Ronit <ronitkbiswas@gmail.com>
2026-04-03 11:53:39 +0530

    final push for now

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -3,11 +3,15 @@ export default function AboutMe() {
         <>
         <h2>📌 About Me</h2>
         <br></br>
-        <span style={{fontSize:"15px", fontFamily: "monospace"}}>🍷 10+ Years of experience in FrontEnd Web Development<br></br> <br></br>(Of course started with html-css-javascript<br></br> & some early-age curiosity about the web ! )
+<span style={{fontSize:"15px", fontFamily: "monospace"}}>🍷 5+ Years of experience in FrontEnd Web Development
+    <br></br>
+    <br></br>
+    (Of course started with html-css-javascript
+    <br></br> & some early-age curiosity about the web ! )
 <br></br><br></br>
 <span>🟢 Busy with ReactJS & building projects!</span>
 <br></br><br></br>
-<span>🍦 Passionate Coder | Engineer (B.Tech 2017) 🚀</span>
+<span>🍦 Passionate Coder | Engineer (B.Tech, 2017) 🚀</span>
         </span>
         </>
     );
