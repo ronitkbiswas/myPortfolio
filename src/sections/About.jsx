662d75ca5b8472b374c9ceb734fdf8c38d987b86
Ronit <ronitkbiswas@gmail.com>
2026-04-04 23:47:56 +0530

    Rearrange AboutMe section content for clarity and emphasis

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -3,15 +3,17 @@ export default function AboutMe() {
         <>
         <h2>📌 About Me</h2>
         <br></br>
-<span style={{fontSize:"15px", fontFamily: "monospace"}}>🍷 5+ Years of experience in FrontEnd Web Development
+        <span>🍦 Passionate Coder | Engineer (B.Tech, 2017) 🚀</span>
+        <br></br><br></br>
+<span style={{fontSize:"15px", fontFamily: "monospace"}}>🍷 Experienced in FrontEnd Web Development
     <br></br>
     <br></br>
     (Of course started with html-css-javascript
     <br></br> & some early-age curiosity about the web ! )
 <br></br><br></br>
 <span>🟢 Busy with ReactJS & building projects!</span>
-<br></br><br></br>
-<span>🍦 Passionate Coder | Engineer (B.Tech, 2017) 🚀</span>
+<br></br> 
+
         </span>
         </>
     );
