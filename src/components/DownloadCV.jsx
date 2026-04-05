86a97a688ccef246fb4d04a0666f9059d2fe639d
Ronit <ronitkbiswas@gmail.com>
2026-04-05 20:58:39 +0530

    huge css changes

--- src/components/DownloadCV.jsx
+++ src/components/DownloadCV.jsx
@@ -11,22 +11,33 @@ function DownloadCV() {
   return (
     <>
       <br></br><br></br>
-<a
-style={{fontSize:"16px",textDecoration: "none"}}
-  href="/cv/ronitkbiswas_2026.pdf"
-  download
-  className="downloadCVButton"
-  onClick={notify}
->
-  Download my latest CV ({new Date().getFullYear()}) ⏬
-  
-</a> <br></br><br></br>
-<a href="tel:+919804727175" className="downloadCVButton" style={{fontSize:"16px",textDecoration: "none"}}>📞 Contact Me</a>
-<br></br>
 
-<a target="_blank" href="https://api.whatsapp.com/send?phone=+919804727175">
-<img style={{paddingTop: "10px", paddingBottom: "5px"}} src={whatsAppicon} width="40px" />
- </a>
+<div className="btnContainer">
+  <a
+    href="/cv/ronitkbiswas_2026.pdf"
+    download
+    onClick={notify}
+    className="callBtn"
+  >
+    📄 Download CV ({new Date().getFullYear()})
+  </a>
+
+  <a
+  href="tel:+919804727175"
+  className="callBtn"
+>
+  <span className="icon">📞</span>
+  <span>Call Me</span>
+</a>
+</div>
+
+<a
+  target="_blank"
+  href="https://api.whatsapp.com/send?phone=+919804727175"
+  className="whatsappFloat"
+>
+  <img src={whatsAppicon} alt="WhatsApp" />
+</a>
        <br></br><hr></hr><br></br>
       
       <ToastContainer />
