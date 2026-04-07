72e97dce25b622ae3aa850d53104f564a1b76b69
Ronit <ronitkbiswas@gmail.com>
2026-04-07 23:32:01 +0530

    social changes

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,32 +1,31 @@
+
+import "/src/App.css";
 import linkedinIcon from "../assets/images/linkedin.png";
-import githubIcon from "../assets/images/github.jpg";
+// import githubIcon from "../assets/images/github.jpg";
 import fbIcon from "../assets/images/facebook.png";
-function SocialLinks() {
+export default function SocialLinks() {
   const fb = "https://facebook.com/ronitkbiswas";
   const github = "https://github.com/ronitkbiswas";
   const linkedin = "https://linkedin.com/in/ronitkbiswas/";
+
   return (
-    <>
-      <div>
-        <span style={{fontSize:"25px",fontWeight:"bold"}}>🌐 Socials</span>
-        <br></br><br></br>
-        <a href={linkedin} target="_blank">
-          <img
-            src={linkedinIcon}
-            alt="linkedin link logo"
-            width="30px"
-          />
-        </a>
-        <span style={{paddingLeft: "5px"}}>  </span> 
-        <a href={github} target="_blank">
-          <img src={githubIcon} alt="github link logo" width="61px" />
-        </a>
-        <span style={{paddingLeft: "5px"}}>  </span> 
-        <a href={fb} target="_blank">
-          <img src={fbIcon} alt="fb link logo" width="30px" />
-        </a>
+    <div className="socialSection">
+      <h2>🌐 Socials</h2>
+
+      <div className="socialIcons">
+        <a href={linkedin} target="_blank" rel="noreferrer">
+  <img src={linkedinIcon} alt="linkedin" />
+</a>
+
+        <a href={github} target="_blank" rel="noreferrer">
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" 
+  style={{ filter: "invert(1)" }}  alt="github" className="githubIcon" />
+</a>
+
+      <a href={fb} target="_blank" rel="noreferrer">
+  <img src={fbIcon} alt="facebook" />
+</a>
       </div>
-    </>
+    </div>
   );
 }
-export default SocialLinks;
